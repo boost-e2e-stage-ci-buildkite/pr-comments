47d2f4e99e0cbae4b8c85a51b112cb47a6faa8c3
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-07-14 12:42:18 +0000

    Rename logs bucket prefix

--- goat.tf
+++ goat.tf
@@ -1,5 +1,5 @@
 
-# Goat.tf version 0.8637669043944503
+# Goat.tf version 0.8491583529375595
 resource "aws_s3_bucket" "flowbucket" {
 bucket        = "${local.resource_prefix.value}-flowlogs"
 force_destroy = true
@@ -9,6 +9,6 @@ Environment = local.resource_prefix.value
 }
 logging {
 target_bucket = "${aws_s3_bucket.logs.id}"
-target_prefix = "log/"
+target_prefix = "log/flowbucket"
 }
 }
