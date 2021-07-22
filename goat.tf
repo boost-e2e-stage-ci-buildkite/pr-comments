2e50a26fae7438db19df638dc70a0e11d7ee99d6
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-07-22 07:53:14 +0000

    Remove public access and add some bit of code

--- goat.tf
+++ goat.tf
@@ -1,5 +1,5 @@
 
-# Goat.tf version 0.44652459473687744
+# Goat.tf version 0.6351541766844111
 resource "google_storage_bucket" "sensitive-data" {
 name          = "secrets.example.com"
 location      = "US"
@@ -16,9 +16,4 @@ member = "allUsers"
 resource "google_storage_bucket" "some-other-data" {
 name          = "another-secrets-bucket.example.com"
 location      = "US"
-}
-resource "google_storage_bucket_iam_member" "some-other-public-access" {
-bucket = google_storage_bucket.some-other-data.name
-role = "roles/storage.objectViewer"
-member = "allUsers"
 }
