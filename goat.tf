acf3f6bc32bb93582e7e7735708d11ce3b97ffaf
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-07-26 22:09:59 +0000

    Add GCS bucket without logging

---
+++ goat.tf
@@ -0,0 +1,11 @@
+
+# Goat.tf version 0.901314483073812
+resource "google_storage_bucket" "sensitive-data" {
+name          = "secrets.example.com"
+location      = "US"
+}
+resource "google_storage_bucket_iam_member" "public-access" {
+bucket = google_storage_bucket.sensitive-data.name
+role = "roles/storage.objectViewer"
+member = "allUsers"
+}
