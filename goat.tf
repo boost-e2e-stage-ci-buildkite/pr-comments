705ed3ba020d0f9b0008593545117ecb89e5f393
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-08-05 17:04:32 +0000

    Rename logs bucket prefix

--- goat.tf
+++ goat.tf
@@ -1,11 +1,11 @@
 
-# Goat.tf version 0.941702978545476
+# Goat.tf version 0.28831278276660277
 resource "google_storage_bucket" "sensitive-data" {
 name          = "secrets.example.com"
 location      = "US"
 logging {
 log_bucket = "secret.example.com-access-logs"
-log_object_prefix =  "log/secrets-"
+log_object_prefix =  "log/some-other-secrets-"
 }
 }
 resource "google_storage_bucket_iam_member" "public-access" {
