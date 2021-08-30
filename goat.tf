eda476a1a9bebee695c37fcc8a4657d38f0081c2
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-08-30 02:26:09 +0000

    Re-Add GCS logging

--- goat.tf
+++ goat.tf
@@ -1,8 +1,12 @@
 
-# Goat.tf version 0.8259787887115151
+# Goat.tf version 0.03760451332029313
 resource "google_storage_bucket" "sensitive-data" {
 name          = "secrets.example.com"
 location      = "US"
+logging {
+log_bucket = "secret.example.com-access-logs"
+log_object_prefix =  "log/some-other-secrets-"
+}
 }
 resource "google_storage_bucket_iam_member" "public-access" {
 bucket = google_storage_bucket.sensitive-data.name
