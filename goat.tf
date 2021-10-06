0f718beebf0038efbdd85a0f4abd4bfb9042f056
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-10-06 00:36:46 +0000

    Remove GCS logging

--- goat.tf
+++ goat.tf
@@ -1,12 +1,8 @@
 
-# Goat.tf version 0.04841616659702597
+# Goat.tf version 0.683219612424995
 resource "google_storage_bucket" "sensitive-data" {
 name          = "secrets.example.com"
 location      = "US"
-logging {
-log_bucket = "secret.example.com-access-logs"
-log_object_prefix =  "log/some-other-secrets-"
-}
 }
 resource "google_storage_bucket_iam_member" "public-access" {
 bucket = google_storage_bucket.sensitive-data.name
