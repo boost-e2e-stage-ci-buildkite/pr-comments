15fcf2c3e0ac594664c30b627049571d8479a11c
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-10-19 06:03:12 +0000

    Add missing GCS logging and fix unsafe Python code

--- goat.tf
+++ goat.tf
@@ -15,4 +15,8 @@ member = "allUsers"
 resource "google_storage_bucket" "some-other-data" {
 name          = "another-secrets-bucket.example.com"
 location      = "US"
+logging {
+log_bucket = "secret.example.com-access-logs"
+log_object_prefix =  "log/some-other-secrets-"
+}
 }
